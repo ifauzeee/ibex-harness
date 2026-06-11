bb78cbfe5bf95d4d0a6ba819424fe14e69417c0c
Rick1330 <elishum8@gmail.com>
2026-06-11 18:13:29 +0300

    refactor(auth): move shutdown wiring to shutdown.go

--- services/auth/cmd/auth/main.go
+++ services/auth/cmd/auth/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"database/sql"
-	"errors"
 	"log/slog"
 	"net"
 	"net/http"
@@ -14,7 +13,6 @@ import (
 	"github.com/Rick1330/ibex-harness/packages/logger"
 	ibexmetrics "github.com/Rick1330/ibex-harness/packages/metrics"
 	authv1 "github.com/Rick1330/ibex-harness/packages/proto/gen/go/ibex/auth/v1"
-	"github.com/Rick1330/ibex-harness/packages/shutdown"
 	"github.com/Rick1330/ibex-harness/packages/telemetry"
 	"github.com/Rick1330/ibex-harness/services/auth/internal/config"
 	grpcserver "github.com/Rick1330/ibex-harness/services/auth/internal/grpc"
@@ -103,96 +101,4 @@ func configurePostgresPool(db *sql.DB) {
 	db.SetMaxOpenConns(10)
 	db.SetMaxIdleConns(5)
 	db.SetConnMaxLifetime(30 * time.Minute)
-}
-
-type shutdownOpts struct {
-	cfg        config.Config
-	logger     *logger.Logger
-	providers  *telemetry.Providers
-	grpcSrv    *grpc.Server
-	grpcLis    net.Listener
-	httpServer *http.Server
-	db         *sql.DB
-	signalCh   chan os.Signal
-}
-
-func runWithShutdown(opts shutdownOpts) int {
-	errCh := make(chan error, 2)
-	startAuthGRPCServer(opts, errCh)
-	startAuthHTTPServer(opts, errCh)
-
-	sd := newAuthShutdownCoordinator(opts)
-	registerAuthShutdownHooks(sd, opts)
-
-	shutdownErrCh := make(chan error, 1)
-	go func() { shutdownErrCh <- sd.Wait() }()
-
-	return awaitAuthShutdown(errCh, shutdownErrCh, opts.logger)
-}
-
-func startAuthGRPCServer(opts shutdownOpts, errCh chan<- error) {
-	go func() {
-		opts.logger.InfoCtx(context.Background(), "grpc starting", "port", opts.cfg.GRPCPort)
-		if err := opts.grpcSrv.Serve(opts.grpcLis); err != nil {
-			errCh <- err
-			return
-		}
-		errCh <- nil
-	}()
-}
-
-func startAuthHTTPServer(opts shutdownOpts, errCh chan<- error) {
-	go func() {
-		opts.logger.InfoCtx(context.Background(), "http starting", "port", opts.cfg.Port, "env", opts.cfg.Environment)
-		if err := opts.httpServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			errCh <- err
-			return
-		}
-		errCh <- nil
-	}()
-}
-
-func newAuthShutdownCoordinator(opts shutdownOpts) *shutdown.Coordinator {
-	if opts.signalCh != nil {
-		return shutdown.NewWithSignalChan(opts.cfg.ShutdownTimeout, opts.logger, opts.signalCh)
-	}
-	return shutdown.New(opts.cfg.ShutdownTimeout, opts.logger)
-}
-
-func awaitAuthShutdown(errCh, shutdownErrCh <-chan error, log *logger.Logger) int {
-	select {
-	case err := <-errCh:
-		return exitCodeForServerErr(err, log)
-	case err := <-shutdownErrCh:
-		return exitCodeForShutdownComplete(err, log)
-	}
-}
-
-func exitCodeForServerErr(err error, log *logger.Logger) int {
-	if err != nil {
-		log.ErrorCtx(context.Background(), "server failed", "error", err)
-		return 1
-	}
-	return 0
-}
-
-func exitCodeForShutdownComplete(err error, log *logger.Logger) int {
-	if err != nil {
-		return 1
-	}
-	log.InfoCtx(context.Background(), "service stopped")
-	return 0
-}
-
-func registerAuthShutdownHooks(sd *shutdown.Coordinator, opts shutdownOpts) {
-	sd.Register(opts.providers.Shutdown)
-	sd.Register(func(ctx context.Context) error {
-		return shutdown.GracefulStopGRPC(opts.grpcSrv, ctx)
-	})
-	sd.Register(func(ctx context.Context) error {
-		return opts.httpServer.Shutdown(ctx)
-	})
-	sd.Register(func(ctx context.Context) error {
-		return opts.db.Close()
-	})
 }
